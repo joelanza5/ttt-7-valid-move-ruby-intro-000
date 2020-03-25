533bc1b27e508085abb7d9eea79ca1d2f052f938
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-25 21:38:11 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -2,14 +2,6 @@ def valid_move?(board, index)
   if board[index] == " " || "" || nil
     true
   end
-  if index.between?(0 .. 8) == true
-    true
-  else
-    false
-  end
-  if board[index] == "X" || "O"
-    false
-  end
 end
 
 def position_taken?(board, index)
