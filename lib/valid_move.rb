2320317e68b40ce12e5cfb15aa1004c860fed57f
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-25 22:15:45 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,5 +1,5 @@
 def valid_move?(board, index)
-  if position_taken?(board, index) == false && index.between?(0 .. 8) == true
+  if position_taken?(board, index) == false && index.between?(0, 8) == true
     true
   else
     false
