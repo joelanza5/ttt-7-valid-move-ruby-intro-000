af3e6b1196f970568b3332a59a6b697f430215ea
JOSEPH LANZA <joelanza5@gmail.com>
2020-03-25 21:22:44 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,9 +1,4 @@
 def valid_move?(board, index)
-  if board[index] == false && index.between(0 .. 8) == true
-    true
-  elsif board[index] == false && index.between(0 .. 8) == false
-    false
-  end
   if board[index] == true
     false
   end
